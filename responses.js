ce0197a2ff84fc2049bac736571708d3d6022c7c
SkyCopeland <sky@skycopeland.com>
2021-03-24 13:01:56 -0400

    Potential Fix for Literals

--- responses.js
+++ responses.js
@@ -11,16 +11,16 @@ export default
             very long time to finish and release. Even trying to make a game like Dead Cells takes a LOT of 
             commitment from a very talented team (that was getting paid). It's far too likely that you will garner 
             a lot of attention from people new to the industry who dream of also making an MMO, but they will 
-            also likely abandon the project with months, or more likely weeks if not days. \n\n 
-            Instead, I would recommend looking to make a very small scoped game, something you'd consider a like 
+            also likely abandon the project with months, or more likely weeks if not days. \n\n` + 
+            `Instead, I would recommend looking to make a very small scoped game, something you'd consider a like 
             a minigame. That way you and anyone who is willing to join the project can gain experience of what 
             it's like to finish a game from design to release. As well it'll give you a better idea of how long 
             it takes to make a game. Not to mention, past shipped projects will give you credibility in future 
             posts on INAT. \n\n`,
         url: `I noticed you don't have any URLs in your submission? If you've worked on any games in the past or 
             have a portfolio, posting a link to them would greatly increase your odds of successfully finding 
-            collaborators here on r/INAT. \n\n 
-            If not, then I would highly recommend making anything even something super small that would show to 
+            collaborators here on r/INAT. \n\n` + 
+            `If not, then I would highly recommend making anything even something super small that would show to 
             potential collaborators that you're serious about gamedev. It can be anything from a simple 
             brick-break game with bad art, sprite sheets of a small character, or 1 minute music loop. \n\n`,
         scope: `This automated response is because the above user believes that your proposed project is larger than 
@@ -29,7 +29,7 @@ export default
             or another. In order to reduce the possibility of this happening it is highly recommended that you 
             scope your project down to a more realistic amount of work for first-time collaboration. Remember 
             the goal of a first-time collab is to form a relationship with your teammates and see if you would 
-            work well together in a larger project. \n\n
-            Here's a video that could perhaps point you in the right direction: 
+            work well together in a larger project. \n\n` +
+            `Here's a video that could perhaps point you in the right direction: 
             https://www.youtube.com/watch?v=UvCri1tqIxQ \n\n`
     }
